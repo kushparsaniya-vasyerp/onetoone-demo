dc23331ecc0429bd06c338560ced2541681755ee
kushparsaniya-vasyerp <kushparsaniya@vasyerp.com>
2024-02-02 16:05:25 +0530

    add same data

--- src/main/java/dev/kush/onetoone/config/SampleData.java
+++ src/main/java/dev/kush/onetoone/config/SampleData.java
@@ -1,6 +1,11 @@
 package dev.kush.onetoone.config;
 
+import dev.kush.onetoone.model.Laptop;
+import dev.kush.onetoone.model.Student;
+import dev.kush.onetoone.repository.LaptopRepository;
+import dev.kush.onetoone.repository.StudentRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +15,17 @@ public class SampleData {
     @Bean
     ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository, LaptopRepository laptopRepository){
+        return args -> {
+
+            Laptop laptop = new Laptop("hp","8GB","i5","50000");
+            laptopRepository.save(laptop);
+
+            Student student = new Student("Kush","kush@gmail.com","1234567889",laptop);
+            studentRepository.save(student);
+        };
     }
 }
